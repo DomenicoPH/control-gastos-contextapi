99fc41ef3fdcc1a30a6cf362a140a465eaac91ed
Domenico Pagano <dpaganoh@gmail.com>
2025-12-26 20:05:13 -0500

    useBudget() action.type show-modal

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,12 +1,15 @@
 export type BudgetActions = 
-    { type: 'add-budget', payload: {budget: number} }
+    { type: 'add-budget', payload: {budget: number} } |
+    { type: 'show-modal' }
 
 export type BudgetState = {
     budget: number
+    modal: boolean
 }
 
 export const initialState : BudgetState = {
-    budget: 0
+    budget: 0,
+    modal: false
 }
 
 export const budgetReducer = (state : BudgetState = initialState, action : BudgetActions) => {
@@ -17,5 +20,13 @@ export const budgetReducer = (state : BudgetState = initialState, action : Budge
             budget: action.payload.budget
         }
     }
+
+    if(action.type === 'show-modal'){
+        return {
+            ...state,
+            modal: true
+        }
+    }
+
     return state;
 }
