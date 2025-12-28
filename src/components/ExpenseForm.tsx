16a4fbdd1f8035a2884ecd52782d787471df4e63
Domenico Pagano <dpaganoh@gmail.com>
2025-12-27 19:33:19 -0500

    ErrorMessage

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -6,6 +6,8 @@ import type { Value } from "../types"
 import 'react-date-picker/dist/DatePicker.css'
 import 'react-calendar/dist/Calendar.css'
 import DatePicker from 'react-date-picker'
+import type { FormEvent } from "react"
+import ErrorMessage from "./ErrorMessage"
 
 export default function ExpenseForm() {
 
@@ -15,6 +17,8 @@ export default function ExpenseForm() {
         category: '',
         date: new Date()
     });
+
+    const [error, setError] = useState('');
 
     const handleChange = (e : ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
         const {name, value } = e.target
@@ -32,12 +36,28 @@ export default function ExpenseForm() {
         })
     }
 
+    const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        //validation
+        if(Object.values(expense).includes('')){
+            setError('Todos los campos son obligatorios')
+            return
+        }
+        console.log('todo bien...')
+    }
+
   return (
-    <form className="space-y-5">
+    <form 
+        className="space-y-5"
+        onSubmit={handleSubmit}
+    >
 
         <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
             Nuevo Gasto
         </legend>
+
+        {error && <ErrorMessage>{error}</ErrorMessage>}
 
         <div className="flex flex-col gap-2">
             <label htmlFor="expenseName" className="text-xl">Nombre Gasto:</label>
