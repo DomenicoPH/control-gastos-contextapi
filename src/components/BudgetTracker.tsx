1fe2b5d62df5a09c251bdb3af8060721cc8a92a9
Domenico Pagano <dpaganoh@gmail.com>
2026-01-02 11:43:33 -0500

    cálculo de presupuesto desde el estado en BudgetTracker

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,34 +1,34 @@
-import AmountDisplay from "./AmountDisplay"
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
+import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
+
+  const { state } = useBudget();
+
+  const totalExpenses = useMemo( () => state.expenses.reduce((total, expense) => expense.amount + total, 0), [state.expenses] );
+
+  const availableBudget = state.budget - totalExpenses;
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <div className="flex justify-center">
-            <img src="/grafico.jpg" alt="Gráfica de gastos" />
-        </div>
-        <div className="flex flex-col justify-center items-center gap-8">
-            <button
-                type="button"
-                className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
-            >
-                Reset App
-            </button>
+      <div className="flex justify-center">
+        <img src="/grafico.jpg" alt="Gráfica de gastos" />
+      </div>
+      <div className="flex flex-col justify-center items-center gap-8">
+        <button
+          type="button"
+          className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+        >
+          Reset App
+        </button>
 
-            <AmountDisplay 
-                label="Presupuesto"
-                amount={300}
-            />
+        <AmountDisplay label="Presupuesto" amount={state.budget} />
 
-            <AmountDisplay 
-                label="Disponible"
-                amount={200}
-            />
+        <AmountDisplay label="Disponible" amount={availableBudget} />
 
-            <AmountDisplay 
-                label="Gastado"
-                amount={100}
-            />
-        </div>
+        <AmountDisplay label="Gastado" amount={totalExpenses} />
+      </div>
     </div>
-  )
+  );
 }
